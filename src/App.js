2e75691901449563f348c134a7773fb2fe008755
Edson Lourenço <edson.lourenco@outlook.com>
2019-12-31 16:59:03 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Routes from './routes';
+
+import Header from './components/Header';
+import Main from './pages/main';
+
+
+import "./styles.css"
+
+const App = () => (
+  <div className='App'>
+    <Header />
+    <Main />
+  </div>
+)
+
+export default App;
